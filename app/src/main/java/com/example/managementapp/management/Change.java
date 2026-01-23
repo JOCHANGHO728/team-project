06833719230d73d80a6e127dcfc1b30549df3589
bkc1021 <62417843+bkc1021@users.noreply.github.com>
2026-01-23 16:28:01 +0900

    상품 수량 변경파일 형식

--- app/src/main/java/com/example/managementapp/management/Change.java
+++ app/src/main/java/com/example/managementapp/management/Change.java
@@ -9,14 +9,33 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.managementapp.R;
+import com.example.managementapp.databinding.ActivityChangeBinding;
+import com.example.managementapp.databinding.ActivityManagementBinding;
 
 public class Change extends AppCompatActivity {
+    private ActivityChangeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_change);
+        binding = ActivityChangeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        //검색 기능
+        binding.search.setOnClickListener(v -> {
+
+        });
+
+        //수량 변경 기능
+        binding.change.setOnClickListener(v -> {
+
+        });
+
+        //관리 화면으로 돌아가기
+        binding.out.setOnClickListener(v -> {
+
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
