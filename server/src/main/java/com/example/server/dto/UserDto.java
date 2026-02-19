28be59059d6db3acea2a796765429f86c984e494
JOCHANGHO728 <148441845+JOCHANGHO728@users.noreply.github.com>
2026-02-19 21:22:16 +0900

    Add files via upload

---
+++ server/src/main/java/com/example/server/dto/UserDto.java
@@ -0,0 +1,16 @@
+public class UserDto {
+    private String loginId;
+    private String password;
+    private String name;
+    private String pNumber;
+
+    // Getter & Setter
+    public String getLoginId() { return loginId; }
+    public void setLoginId(String loginId) { this.loginId = loginId; }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public String getPNumber() { return pNumber; }
+    public void setPNumber(String pNumber) { this.pNumber = pNumber; }
+}
