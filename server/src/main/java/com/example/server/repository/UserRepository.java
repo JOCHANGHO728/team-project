8e9c9aab65b77e648f0a8f55eaf485c3fee079b7
JOCHANGHO728 <148441845+JOCHANGHO728@users.noreply.github.com>
2026-02-19 22:16:56 +0900

    Update UserRepository.java

--- server/src/main/java/com/example/server/repository/UserRepository.java
+++ server/src/main/java/com/example/server/repository/UserRepository.java
@@ -13,9 +13,9 @@ public class UserRepository {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    // 1. 회원가입 (userdb 추가)
+    // 1. 회원가입 (userdb 추가) - 백틱(`)을 추가하여 문법 에러 방지
     public int insertUser(UserDto user) {
-        String sql = "INSERT INTO userdb (login_id, password, name, p_number) VALUES (?, ?, ?, ?)";
+        String sql = "INSERT INTO userdb (`login_id`, `password`, `name`, `p_number`) VALUES (?, ?, ?, ?)";
         return jdbcTemplate.update(sql, user.getLoginId(), user.getPassword(), user.getName(), user.getPNumber());
     }
 
