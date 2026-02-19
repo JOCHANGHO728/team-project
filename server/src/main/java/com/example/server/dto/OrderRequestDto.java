7af47277c3f3331d7a17cfbee7f009c60e5fbf4c
JOCHANGHO728 <148441845+JOCHANGHO728@users.noreply.github.com>
2026-02-19 21:47:39 +0900

    Update OrderRequestDto.java

--- server/src/main/java/com/example/server/dto/OrderRequestDto.java
+++ server/src/main/java/com/example/server/dto/OrderRequestDto.java
@@ -1,10 +1,11 @@
+package com.example.server.dto;
+
 import java.util.List;
 
 public class OrderRequestDto {
     private Long customerId;
-    private List<CartItemDto> cartItems; // 장바구니에 담긴 여러 상품들
+    private List<CartItemDto> cartItems;
 
-    // Getter & Setter
     public Long getCustomerId() { return customerId; }
     public void setCustomerId(Long customerId) { this.customerId = customerId; }
     public List<CartItemDto> getCartItems() { return cartItems; }
