7c15b417c2712884beeaa7f705f412ec904ebd25
JOCHANGHO728 <148441845+JOCHANGHO728@users.noreply.github.com>
2026-02-19 22:33:11 +0900

    Update UserRepository.java

--- server/src/main/java/com/example/server/repository/UserRepository.java
+++ server/src/main/java/com/example/server/repository/UserRepository.java
@@ -15,13 +15,13 @@ public class UserRepository {
 
     // 1. 회원가입 (userdb 추가) - 백틱(`)을 추가하여 문법 에러 방지
     public int insertUser(UserDto user) {
-        String sql = "INSERT INTO userdb (`login_id`, `password`, `name`, `p_number`) VALUES (?, ?, ?, ?)";
+        String sql = "INSERT INTO usersdb (`login_id`, `password`, `name`, `p_number`) VALUES (?, ?, ?, ?)";
         return jdbcTemplate.update(sql, user.getLoginId(), user.getPassword(), user.getName(), user.getPNumber());
     }
 
     // 2. 회원탈퇴 (userdb 삭제)
     public int deleteUser(Long id) {
-        String sql = "DELETE FROM userdb WHERE id = ?";
+        String sql = "DELETE FROM usersdb WHERE id = ?";
         return jdbcTemplate.update(sql, id);
     }
 }
