2bba9b6ab5dadf96a5d437e2b0a729d2784e7b96
JOCHANGHO728 <148441845+JOCHANGHO728@users.noreply.github.com>
2026-03-01 20:49:56 +0900

    Merge remote-tracking branch 'origin/test-server' into test-server

--- server/src/main/java/com/example/server/repository/ProductRepository.java
+++ server/src/main/java/com/example/server/repository/ProductRepository.java
@@ -2,8 +2,14 @@ package com.example.server.repository;
 
 import com.example.server.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    List<Product> findByPNameContaining(String keyword);
+
+    @Query("SELECT p FROM Product p WHERE p.pName LIKE %:keyword%")
+    List<Product> findByPNameContaining(@Param("keyword") String keyword);
+
 }
