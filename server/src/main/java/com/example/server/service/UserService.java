081f549800408731ee4776cc623d98991c6603ad
leesungjae <2023150040@tukorea.ac.kr>
2026-03-10 11:48:39 +0900

    Add files via upload

---
+++ server/src/main/java/com/example/server/service/UserService.java
@@ -0,0 +1,28 @@
+package com.example.server.service;
+
+import com.example.server.dto.UserLoginDto;
+import com.example.server.entity.User;
+import com.example.server.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public String login(UserLoginDto request) {
+
+        User user = userRepository.findByUserId(request.getUserId())
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 고객 아이디입니다."));
+
+
+        if (!user.getUPassword().equals(request.getUPassword())) {
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+
+
+        return user.getUName() + "님, 환영합니다!";
+    }
+}
