76df14ca8215b0ecf0d93279dabf5e168fef12b7
leesungjae <2023150040@tukorea.ac.kr>
2026-03-10 15:45:05 +0900

    Update UserRepository.java

--- server/src/main/java/com/example/server/repository/UserRepository.java
+++ server/src/main/java/com/example/server/repository/UserRepository.java
@@ -4,8 +4,7 @@ import com.example.server.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
-
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findByLoginId(String loginId);
+    Optional<User> findByUId(String uId);
 }
