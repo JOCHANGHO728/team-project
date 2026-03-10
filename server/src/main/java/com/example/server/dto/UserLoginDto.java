dc0e11c3d76a4db70ca26f718bcd3b95cade5f39
leesungjae <2023150040@tukorea.ac.kr>
2026-03-10 14:57:45 +0900

    Refactor UserLoginDto fields for clarity

--- server/src/main/java/com/example/server/dto/UserLoginDto.java
+++ server/src/main/java/com/example/server/dto/UserLoginDto.java
@@ -5,6 +5,6 @@ import lombok.Setter;
 
 @Getter @Setter
 public class UserLoginDto {
-    private String userId;    // 로그인 아이디를 담을 칸
-    private String uPassword; // 비밀번호를 담을 칸
+    private String login_id;
+    private String password;
 }
