b2c8435db4f3d8ac5b3f91a489bb85956fdc545c
leesungjae <2023150040@tukorea.ac.kr>
2026-03-11 14:08:41 +0900

    Add files via upload

--- server/src/main/java/com/example/server/controller/ProductApiController.java
+++ server/src/main/java/com/example/server/controller/ProductApiController.java
@@ -1,5 +1,6 @@
 package com.example.server.controller;
 
+import com.example.server.dto.ProductResponseDto;
 import com.example.server.entity.Product;
 import com.example.server.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
@@ -7,21 +8,26 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api/v1/products") 
+@RequestMapping("/api/v1/products")
 @RequiredArgsConstructor
 public class ProductApiController {
 
     private final ProductRepository productRepository;
 
-    // 상품 이름으로 검색하는 API
-    // 실행 주소: GET http://localhost:8080/api/v1/products/search?keyword=새우깡
     @GetMapping("/search")
-    public ResponseEntity<List<Product>> searchProduct(@RequestParam("keyword") String keyword) {
-        
-        List<Product> products = productRepository.searchByName(keyword);
-        return ResponseEntity.ok(products);
-    }
+    public ResponseEntity<List<ProductResponseDto>> searchProduct(@RequestParam("keyword") String keyword) {
 
+        List<Product> products = productRepository.searchByName(keyword);
+
+
+        List<ProductResponseDto> responseDtos = products.stream()
+                .map(ProductResponseDto::new)
+                .collect(Collectors.toList());
+
+
+        return ResponseEntity.ok(responseDtos);
+    }
 }
