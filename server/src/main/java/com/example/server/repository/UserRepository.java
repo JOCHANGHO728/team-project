c425e82b44b72b6ee34d6543662b6a0e8eb39475
JOCHANGHO728 <148441845+JOCHANGHO728@users.noreply.github.com>
2026-03-13 16:31:47 +0900

    Update UserSignupDto.java

--- server/src/main/java/com/example/server/repository/UserRepository.java
+++ server/src/main/java/com/example/server/repository/UserRepository.java
@@ -6,5 +6,5 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findByuId(String uId);
+    Optional<User> findByUId(String uId);
 }
