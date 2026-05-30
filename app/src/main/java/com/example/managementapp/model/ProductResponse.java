11dfddd3528662e95b27b1573b908f510e11c3ab
JOCHANGHO728 <148441845+JOCHANGHO728@users.noreply.github.com>
2026-05-30 18:07:21 +0900

    ProductResponse.java 수정 및 로그인 확인

--- app/src/main/java/com/example/managementapp/model/ProductResponse.java
+++ app/src/main/java/com/example/managementapp/model/ProductResponse.java
@@ -1,37 +1,59 @@
 package com.example.managementapp.model;
+
 import com.google.gson.annotations.SerializedName;
 
 import java.io.Serializable;
 
 public class ProductResponse implements Serializable {
 
-    //p_id	p_name	p_price	p_quantity	b_key	category
-
-    @SerializedName("pId")
+    @SerializedName(value = "pId", alternate = {"pid", "p_id"})
     private Long p_id;
 
-    @SerializedName("pName")
+    @SerializedName(value = "pName", alternate = {"pname", "p_name", "name"})
     private String p_name;
 
-    @SerializedName("pPrice")
+    @SerializedName(value = "pPrice", alternate = {"pprice", "p_price", "price"})
     private int p_price;
 
-    @SerializedName("pQuantity")
+    @SerializedName(value = "pQuantity", alternate = {"pquantity", "p_quantity", "quantity"})
     private int p_quantity;
 
-    @SerializedName("bKey")
+    @SerializedName(value = "bKey", alternate = {"bkey", "b_key", "barcode"})
     private String b_key;
 
     @SerializedName("category")
     private String category;
 
-    // 🔥 Getter 추가
-    public Long getP_id() { return p_id; }
-    public String getP_name() { return p_name; }
-    public int getP_price() { return p_price; }
-    public int getP_quantity() { return p_quantity; }
-    public String getB_key() { return b_key; }
-    public String getCategory() { return category; }
+    @SerializedName(value = "imageUrl", alternate = {"imageurl", "image_url"})
+    private String imageUrl;
+
+    public Long getP_id() {
+        return p_id;
+    }
+
+    public String getP_name() {
+        return p_name;
+    }
+
+    public int getP_price() {
+        return p_price;
+    }
+
+    public int getP_quantity() {
+        return p_quantity;
+    }
+
+    public String getB_key() {
+        return b_key;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
 
     @Override
     public String toString() {
@@ -40,8 +62,9 @@ public class ProductResponse implements Serializable {
                 ", p_name='" + p_name + '\'' +
                 ", p_price=" + p_price +
                 ", p_quantity=" + p_quantity +
-                ", b_Key='" + b_key + '\'' +
+                ", b_key='" + b_key + '\'' +
                 ", category='" + category + '\'' +
+                ", imageUrl='" + imageUrl + '\'' +
                 '}';
     }
 }
